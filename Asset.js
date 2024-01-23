097b64fb0745b664ee493d99a2e7bd13db444247
ankitwork9808 <65911596+ankitwork9808@users.noreply.github.com>
2024-01-23 13:33:44 +0530

    VITE_ASSET_URL check update

--- Asset.js
+++ Asset.js
@@ -6,14 +6,14 @@ export const Asset = {
           if(urlRegex.test(src)){
             return src
           }
-          
-          if(import.meta?.env?.VITE_ASSET_URL) {
+          const asset_url = import.meta.env.VITE_ASSET_URL
+          if(asset_url) {
             if(src?.startsWith("/")){
               src = src.substring(1)
             }
-            src = import.meta.env.VITE_ASSET_URL+src;
+            src = asset_url+src;
           }else{
-              console.error('VITE_ASSET_URL is not defined in .env')
+              console.error(`VITE_ASSET_URL("${asset_url}") is not defined in .env`)
           }
 
           return src;
